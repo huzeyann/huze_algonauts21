39212c7e4c4c6f99cc116aef5788690148688ec4
Kotorinyanya <huze@sas.upenn.edu>
2021-07-10 10:10:35 +0800

    v1 x4

--- start_tasks.py
+++ start_tasks.py
@@ -26,10 +26,8 @@ queues_buffer = itertools.cycle(queue_names)
 
 task_ids = []
 
-# pooling_schs = ['no', 'avg']
-pooling_schs = ['']
-global_pooling = True
-
+pooling_schs = ['no', 'avg']
+global_pooling = False
 for pooling_sch in pooling_schs:
     for roi in rois:
         queue = next(queues_buffer)
@@ -38,21 +36,23 @@ for pooling_sch in pooling_schs:
                                  name=template_task.name + f' {roi} {pooling_sch}',
                                  parent=template_task.id)
 
-        cloned_task.add_tags([roi, 'global_pool'])
+        cloned_task.add_tags([roi, 'global_pool', 'x4'])
 
         cloned_task_parameters = cloned_task.get_parameters()
         # cloned_task_parameters['rois'] = [roi]
         cloned_task_parameters['Args/roi'] = roi
         # cloned_task_parameters['Args/batch_size'] = 32 if pooling_sch in ['avg', 'max'] else 24
-        cloned_task_parameters['Args/batch_size'] = 32
+        cloned_task_parameters['Args/batch_size'] = 24
+        cloned_task_parameters['Args/conv_size'] = 512
         cloned_task_parameters['Args/debug'] = False
-        # cloned_task_parameters['Args/early_stop_epochs'] = 15 if pooling_sch in ['avg', 'max'] else 5
-        cloned_task_parameters['Args/early_stop_epochs'] = 15
+        cloned_task_parameters['Args/early_stop_epochs'] = 15 if pooling_sch in ['avg', 'max'] else 5
+        # cloned_task_parameters['Args/early_stop_epochs'] = 15
         cloned_task_parameters['Args/gpus'] = queue.split('-')[1]
         cloned_task_parameters['Args/pooling_mode'] = pooling_sch
         cloned_task_parameters['Args/global_pooling'] = global_pooling
-        # cloned_task_parameters['Args/predictions_dir'] = f'/home/huze/.cache/predictions/v1_{pooling_sch}/'
-        cloned_task_parameters['Args/predictions_dir'] = f'/home/huze/.cache/predictions/v1_global_pool/'
+        cloned_task_parameters['Args/save_checkpoints'] = False
+        # cloned_task_parameters['Args/predictions_dir'] = f'/home/huze/.cache/predictions/v1_x4_{pooling_sch}/'
+        # cloned_task_parameters['Args/predictions_dir'] = f'/home/huze/.cache/predictions/v1_global_pool/'
 
         # put back into the new cloned task
         cloned_task.set_parameters(cloned_task_parameters)
@@ -63,5 +63,42 @@ for pooling_sch in pooling_schs:
         print('Experiment id={} enqueue for execution'.format(cloned_task.id))
 
         task_ids.append(cloned_task.id)
+
+pooling_sch = ''
+global_pooling = True
+for roi in rois:
+    queue = next(queues_buffer)
+
+    cloned_task = Task.clone(source_task=template_task,
+                             name=template_task.name + f' {roi} {pooling_sch}',
+                             parent=template_task.id)
+
+    cloned_task.add_tags([roi, 'global_pool', 'x4'])
+
+    cloned_task_parameters = cloned_task.get_parameters()
+    # cloned_task_parameters['rois'] = [roi]
+    cloned_task_parameters['Args/roi'] = roi
+    # cloned_task_parameters['Args/batch_size'] = 32 if pooling_sch in ['avg', 'max'] else 24
+    cloned_task_parameters['Args/batch_size'] = 32
+    cloned_task_parameters['Args/conv_size'] = 2048
+    cloned_task_parameters['Args/debug'] = False
+    # cloned_task_parameters['Args/early_stop_epochs'] = 15 if pooling_sch in ['avg', 'max'] else 5
+    cloned_task_parameters['Args/early_stop_epochs'] = 15
+    cloned_task_parameters['Args/gpus'] = queue.split('-')[1]
+    cloned_task_parameters['Args/pooling_mode'] = pooling_sch
+    cloned_task_parameters['Args/global_pooling'] = global_pooling
+    cloned_task_parameters['Args/save_checkpoints'] = False
+    # cloned_task_parameters['Args/predictions_dir'] = f'/home/huze/.cache/predictions/v1_{pooling_sch}/'
+    # cloned_task_parameters['Args/predictions_dir'] = f'/home/huze/.cache/predictions/v1_x4_global_pool/'
+
+    # put back into the new cloned task
+    cloned_task.set_parameters(cloned_task_parameters)
+    print('Experiment set with parameters {}'.format(cloned_task_parameters))
+
+    # enqueue the task for execution
+    Task.enqueue(cloned_task.id, queue_name=queue)
+    print('Experiment id={} enqueue for execution'.format(cloned_task.id))
+
+    task_ids.append(cloned_task.id)
 
 print(task_ids)
