1432f50999b3538c940bb5c085bb1f812ab035ec
Kotorinyanya <huze@sas.upenn.edu>
2021-07-23 12:43:26 +0800

    voxel wise debug

--- start_tasks_new.py
+++ start_tasks_new.py
@@ -51,8 +51,8 @@ for fold in range(5):
     # cloned_task_parameters['rois'] = [roi]
     cloned_task_parameters['Args/rois'] = rois
     # cloned_task_parameters['Args/batch_size'] = 32 if pooling_sch in ['avg', 'max'] else 24
-    cloned_task_parameters['Args/batch_size'] = 8
-    cloned_task_parameters['Args/accumulate_grad_batches'] = 4
+    cloned_task_parameters['Args/batch_size'] = 4
+    cloned_task_parameters['Args/accumulate_grad_batches'] = 8
     cloned_task_parameters['Args/num_layers'] = 1
     cloned_task_parameters['Args/conv_size'] = 256
     cloned_task_parameters['Args/layer_hidden'] = 2048
@@ -108,8 +108,8 @@ for fold in range(5):
     # cloned_task_parameters['rois'] = [roi]
     cloned_task_parameters['Args/rois'] = rois
     # cloned_task_parameters['Args/batch_size'] = 32 if pooling_sch in ['avg', 'max'] else 24
-    cloned_task_parameters['Args/batch_size'] = 8
-    cloned_task_parameters['Args/accumulate_grad_batches'] = 4
+    cloned_task_parameters['Args/batch_size'] = 4
+    cloned_task_parameters['Args/accumulate_grad_batches'] = 8
     cloned_task_parameters['Args/num_layers'] = 1
     cloned_task_parameters['Args/conv_size'] = 256
     cloned_task_parameters['Args/layer_hidden'] = 2048
