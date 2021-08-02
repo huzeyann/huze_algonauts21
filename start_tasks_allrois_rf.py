7723b3fc5f7c19e50e428ee74c2be4b4afda5e33
Kotorinyanya <huze@sas.upenn.edu>
2021-08-02 17:03:22 +0800

    hpo debug

--- start_tasks_allrois_rf.py
+++ start_tasks_allrois_rf.py
@@ -27,9 +27,12 @@ task_ids = []
 
 roi = 'V1,V2,V3,V4,EBA,LOC,STS,FFA,PPA'
 rfs = list(range(1, 16 + 1))
-layer = 'x1,x2,x3,x4'
 
 for rf in rfs:
+    if rf > 9:
+        layer = 'x1,x2,x3'
+    else:
+        layer = 'x1,x2,x3,x4'
     queue = next(queues_buffer)
 
     cloned_task = Task.clone(source_task=template_task,
@@ -43,6 +46,7 @@ for rf in rfs:
     cloned_task_parameters['Args/rois'] = roi
     cloned_task_parameters['Args/track'] = 'mini_track'
     # cloned_task_parameters['Args/batch_size'] = 32 if pooling_sch in ['avg', 'max'] else 24
+    cloned_task_parameters['Args/learning_rate'] = 1e-3
     cloned_task_parameters['Args/batch_size'] = 8
     cloned_task_parameters['Args/accumulate_grad_batches'] = 4
     cloned_task_parameters['Args/num_layers'] = 2
@@ -61,10 +65,7 @@ for rf in rfs:
     cloned_task_parameters['Args/pooling_mode'] = 'max'
     for l in layer.split(','):
         cloned_task_parameters[f'Args/{l}_pooling_mode'] = 'adaptive_max'
-        if l == 'x4' and rf > 9:
-            cloned_task_parameters[f'Args/pooling_size_{l}'] = 2
-        else:
-            cloned_task_parameters[f'Args/pooling_size_{l}'] = rf
+        cloned_task_parameters[f'Args/pooling_size_{l}'] = rf
     cloned_task_parameters['Args/backbone_type'] = 'i3d_rgb'
     cloned_task_parameters['Args/final_fusion'] = 'conv'
     cloned_task_parameters['Args/pyramid_layers'] = layer
